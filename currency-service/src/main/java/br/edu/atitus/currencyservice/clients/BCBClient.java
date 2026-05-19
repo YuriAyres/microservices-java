83bd7aaf4fb8104ef4f70ecc49d2ffb3479ed5e5
YuriAyres <yuriadp7@gmail.com>
2026-05-19 17:09:17 -0300

    discovery

---
+++ currency-service/src/main/java/br/edu/atitus/currencyservice/clients/BCBClient.java
@@ -0,0 +1,16 @@
+package br.edu.atitus.currencyservice.clients;
+
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "bcb-client", url="https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata", fallback = BCBClientFallback.class)
+public interface BCBClient {
+    //Consulta Data fixa 15/05/2026
+    @GetMapping("/CotacaoMoedaDia(moeda=@moeda,dataCotacao=@dataCotacao)?%40moeda='{moeda}'&%40dataCotacao='05-15-2026'&%24format=json")
+    @Retry(name = "bcb-client")
+    @CircuitBreaker(name = "bcb-client")
+    BCBResponse getBCBCurrency(@PathVariable String moeda);
+}
